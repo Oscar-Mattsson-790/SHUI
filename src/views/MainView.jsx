b3d29f0555c04be9c5ea7ced0ab1caadd3654ae5
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-11-28 11:03:27 +0100

    fix

--- src/views/MainView.jsx
+++ src/views/MainView.jsx
@@ -1,13 +1,11 @@
 import MessageList from "../components/MessageList/MessageList";
-import MessageInput from "../components/MessageInput/MessageInput";
+
 import "./MainView.css";
-import { Link } from "react-router-dom";
 
 export default function MainView({ messages }) {
   return (
     <div className="Container">
       <MessageList messages={messages} />
-      <MessageInput />
     </div>
   );
 }
