e6824b25c299c1e5f692aca50bfe40816f581341
Oscar Mattsson <60174332+Oscar-Mattsson-790@users.noreply.github.com>
2023-11-28 16:48:57 +0100

    refactor (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,61 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 import MainView from "./views/MainView";
-import PostMessage from "./components/PostMessage/PostMessage";
-import EmptyState from "./components/EmptyState/EmptyState";
 import "./App.css";
 
-function App() {
+const App = () => {
   const [messages, setMessages] = useState([
     {
-      id: uuidv4(),
-      author: "Snodden",
-      content: "Saker händer här och där. Eftersökningar pågår...",
+      id: 0,
+      username: "Bob",
+      text: "Jag saknar sommaren... Saker händer här och där. Elakingar på spårvagnarna vid Brunnsparken. Se till att vara på plats den tiden, var försiktiga.",
+      time: "12:59",
+      date: new Date("2023-06-21"),
     },
     {
-      id: uuidv4(),
-      author: "Lindberg",
-      content: "Nytt möte planerat för nästa vecka. Håll er uppdaterade!",
+      id: 1,
+      username: "Dave",
+      text: "Är det någon som studerar till tentan? Saker händer här och där. Elakingar på spårvagnarna vid Brunnsparken. Se till att vara på plats den tiden, var försiktiga.",
+      time: "12:59",
+      date: new Date("2023-06-21"),
     },
     {
-      id: uuidv4(),
-      author: "Björnsson",
-      content: "Projektet går framåt. Vi behöver mer feedback.",
+      id: 2,
+      username: "Bob",
+      text: "Någon som vill gå och fika? Saker händer här och där. Elakingar på spårvagnarna vid Brunnsparken. Se till att vara på plats den tiden, var försiktiga.",
+      time: "12:59",
+      date: new Date("2023-06-21"),
     },
     {
-      id: uuidv4(),
-      author: "Åkerman",
-      content: "Glöm inte bort deadline för rapporten imorgon!",
+      id: 3,
+      username: "Dave",
+      text: "Någon som vill gå och fika? Saker händer här och där. Elakingar på spårvagnarna vid Brunnsparken. Se till att vara på plats den tiden, var försiktiga.",
+      time: "12:59",
+      date: new Date("2023-06-21"),
     },
     {
-      id: uuidv4(),
-      author: "Svensson",
-      content: "Kaffemaskinen är trasig igen. Någon som kan fixa?",
+      id: 4,
+      username: "Alice",
+      text: "Hej alla, hoppas ni har en bra dag! Saker händer här och där. Elakingar på spårvagnarna vid Brunnsparken. Se till att vara på plats den tiden, var försiktiga.",
+      time: "12:59",
+      date: new Date("2023-06-21"),
     },
   ]);
 
-  const addMessage = (newMessage) => {
-    setMessages([...messages, { ...newMessage, id: uuidv4() }]);
+  const addMessage = (username, text) => {
+    const newMessage = {
+      id: messages.length,
+      username: username,
+      text: text,
+      time: new Date().toLocaleTimeString("sv-SE", {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      date: new Date(),
+    };
+    setMessages([...messages, newMessage]);
   };
 
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <div>
-          <nav className="Nav">
-            <Link to="/SHUI">Hem</Link>
-            <Link to="/SHUI/post">Posta Meddelande</Link>
-            <Link to="/SHUI/empty">Tomt tillstånd</Link>
-          </nav>
-        </div>
-        <Routes>
-          <Route path="/SHUI" element={<MainView messages={messages} />} />
-          <Route
-            path="/SHUI/post"
-            element={<PostMessage onAddMessage={addMessage} />}
-          />
-          <Route path="/SHUI/empty" element={<EmptyState />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
+  return <MainView messages={messages} addMessage={addMessage} />;
+};
 
 export default App;
